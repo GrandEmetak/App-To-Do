d040f0d6f3ecb2dd0131558fdbce1be1ed074f9a
SlartiBartFast-art <JekHamond@tutanota.com>
2021-10-08 20:18:12 +0500

    Travis commit

---
+++ bd/Schema.sql
@@ -0,0 +1,19 @@
+create table items(
+ id serial primary key,
+ description VARCHAR(300),
+ created TIMESTAMP with time zone,
+ done boolean,
+ user_id INT not null references users(id)
+);
+
+create table users(
+ id serial primary key,
+ name VARCHAR(15),
+ email VARCHAR(35),
+ password VARCHAR(15)
+);
+
+create table categories(
+ id serial primary key,
+ name VARCHAR(15)
+);
